d979e24adb0faeb1e5f7c605874e0013824f4170
Ilya <Dragoilya@yandex.ru>
2022-12-15 23:20:25 +0300

    Separated fetching images from nasa epyc.

---
+++ fetch_nasa_epyc_images.py
@@ -0,0 +1,20 @@
+from base_scripts import download_image
+import datetime
+import requests
+
+
+def fetch_nasa_epyc_images(api_token, images_number=10):
+    api_endpoint = "https://api.nasa.gov/EPIC/api/natural"
+    api_archive_endpoint = "https://api.nasa.gov/EPIC/archive/natural/"
+    payload = {"api_key": f"{api_token}"}
+
+    response = requests.get(api_endpoint, params=payload)
+    response.raise_for_status()
+
+    for i in range(images_number):
+        image_id = response.json()[i]["image"]
+        date_time = datetime.datetime.fromisoformat(response.json()[i]["date"])
+        download_image(
+            image_url=f"{api_archive_endpoint}{date_time.year}/{date_time.month}/{date_time.day}/png/{image_id}.png",
+            image_name=f"nasa_epyc_{i}", params=payload
+        )
--- space_chat.py
+++ space_chat.py
@@ -1,26 +1,8 @@
 from base_scripts import download_image
 from fetch_spacex_images import fetch_spacex_images
 from fetch_nasa_apod_images import fetch_nasa_apod_images
+from fetch_nasa_epyc_images import fetch_nasa_epyc_images
 from dotenv import dotenv_values
-import datetime
-import requests
-
-
-def fetch_nasa_epyc_images(api_token, images_number=10):
-    api_endpoint = "https://api.nasa.gov/EPIC/api/natural"
-    api_archive_endpoint = "https://api.nasa.gov/EPIC/archive/natural/"
-    payload = {"api_key": f"{api_token}"}
-
-    response = requests.get(api_endpoint, params=payload)
-    response.raise_for_status()
-
-    for i in range(images_number):
-        image_id = response.json()[i]["image"]
-        date_time = datetime.datetime.fromisoformat(response.json()[i]["date"])
-        download_image(
-            image_url=f"{api_archive_endpoint}{date_time.year}/{date_time.month}/{date_time.day}/png/{image_id}.png",
-            image_name=f"nasa_epyc_{i}", params=payload
-        )
 
 
 def main():
