1b000b4edfbdf9eb68f8d49237d26fc1488363da
Ilya <Dragoilya@yandex.ru>
2022-12-15 23:37:40 +0300

    Added arguments parser.

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,5 +1,12 @@
 from base_scripts import download_image
 import requests
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('news_id', nargs='?')
+    return parser.parse_args()
 
 
 def fetch_spacex_images(news_id="latest"):
@@ -10,5 +17,11 @@ def fetch_spacex_images(news_id="latest"):
         download_image(image_url=image_url, image_name=f"spacex_{iters}")
 
 
+def main():
+    arguments = get_arguments()
+    news_id = arguments.news_id if arguments.news_id else "latest"
+    fetch_spacex_images(news_id=news_id)
+
+
 if __name__ == '__main__':
-    pass
+    main()
