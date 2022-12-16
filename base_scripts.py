cdafd4f3b93cc56afa565b02e431963e06b32f56
Ilya <Dragoilya@yandex.ru>
2022-12-16 16:09:12 +0300

    Refactored base cripts.

--- base_scripts.py
+++ base_scripts.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from urllib.parse import urlparse
 import os
 import requests
@@ -14,11 +13,6 @@ def download_image(image_url, image_name, image_directory="./images", params=Non
     response = requests.get(url=image_url, params=params)
     response.raise_for_status()
 
-    if not os.path.exists(image_directory):
-        Path(image_directory).mkdir(parents=True, exist_ok=True)
+    os.makedirs(image_directory, exist_ok=True)
     with open(f"{image_directory}/{image_name}{get_image_type(image_url)}", 'wb') as fh:
         fh.write(response.content)
-
-
-if __name__ == '__main__':
-    pass
