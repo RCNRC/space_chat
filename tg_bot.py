7702f95d331220623f52df7756bbddc9ce722826
Ilya <Dragoilya@yandex.ru>
2022-12-16 17:24:10 +0300

    Refactored names..

--- tg_bot.py
+++ tg_bot.py
@@ -12,7 +12,7 @@ def get_arguments():
     return parser.parse_args()
 
 
-def posting_random_image(bot, chat_id, hours_num, directory="images", ):
+def post_images(bot, chat_id, hours_num, directory="images", ):
     files = os.walk(f"./{directory}")
     files = next(files)[2]
     shuffle(files)
@@ -20,7 +20,7 @@ def posting_random_image(bot, chat_id, hours_num, directory="images", ):
         bot.send_document(chat_id=chat_id, document=open(f"{directory}/{file}", 'rb'))
         time.sleep(hours_num*60*60)
         hours_num = int(os.getenv('HOURS_NUM')) if os.getenv('HOURS_NUM') else hours_num
-    posting_random_image(bot=bot, chat_id=chat_id, directory=directory, hours_num=hours_num)
+    post_images(bot=bot, chat_id=chat_id, directory=directory, hours_num=hours_num)
 
 
 def main():
@@ -28,7 +28,7 @@ def main():
     bot = telegram.Bot(token=api_key)
     chat_id = dotenv_values(".env")["CHAT_ID"]
     arguments = get_arguments()
-    posting_random_image(bot=bot, chat_id=chat_id, hours_num=int(arguments.hours_num))
+    post_images(bot=bot, chat_id=chat_id, hours_num=int(arguments.hours_num))
 
 
 if __name__ == '__main__':
