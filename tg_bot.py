2aada7e321489383c5aab52547b850873d9b441c
Ilya <Dragoilya@yandex.ru>
2022-12-18 12:38:09 +0300

    Refactored environment arguments parse.

--- tg_bot.py
+++ tg_bot.py
@@ -6,10 +6,6 @@ from random import shuffle
 import time
 
 
-def get_environment_argument(default_value):
-    return int(os.getenv('HOURS_NUM')) if os.getenv('HOURS_NUM') else default_value
-
-
 def get_arguments():
     parser = argparse.ArgumentParser(description="Script starts the telegram bot.")
     parser.add_argument('hours_num', nargs='?', default=4, help="the number of hours between file uploads.")
@@ -25,7 +21,6 @@ def post_images(bot, chat_id, hours_num, directory="images", ):
             document = file_handler.read()
         bot.send_document(chat_id=chat_id, document=document)
         time.sleep(hours_num*60*60)
-        hours_num = get_environment_argument(default_value=hours_num)
     post_images(bot=bot, chat_id=chat_id, directory=directory, hours_num=hours_num)
 
 
@@ -34,7 +29,10 @@ def main():
     bot = telegram.Bot(token=api_key)
     chat_id = dotenv_values(".env")["CHAT_ID"]
     arguments = get_arguments()
-    post_images(bot=bot, chat_id=chat_id, hours_num=int(arguments.hours_num))
+    hours_num = int(arguments.hours_num)
+    if os.getenv('HOURS_NUM'):
+        hours_num = int(os.getenv('HOURS_NUM'))
+    post_images(bot=bot, chat_id=chat_id, hours_num=hours_num)
 
 
 if __name__ == '__main__':
